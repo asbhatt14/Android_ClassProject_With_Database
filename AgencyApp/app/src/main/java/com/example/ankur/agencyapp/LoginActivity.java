f962e617147ac2ea5048cd5465ac4ec8f8cd3887
Ankur Bhatt <30511170+asbhatt14@users.noreply.github.com>
2017-08-02 07:53:17 +0530

    Login Screen
    Need to change few things in Login Screen. Will commit it afterwards.

---
+++ AgencyApp/app/src/main/java/com/example/ankur/agencyapp/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.ankur.agencyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+
+    }
+}
